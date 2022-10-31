207bd506c27d9ff33d6b8c98eae00a7079fd6f87
Logan Yu <loganyu439@gmail.com>
2022-10-31 19:40:21 -0400

    more progress

--- logan-portfolio/src/components/Home/index.js
+++ logan-portfolio/src/components/Home/index.js
@@ -1,22 +1,37 @@
-import LogoTitle from '../../assets/images/logo-s.png';
-import { Link } from 'react-router-dom';
-import './index.scss';
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
+
+
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['L','o','g','a','n']
+    const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
+
 
     return (
-        <div classname="container home-page">
+        <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
-                    <img 
-                        src={LogoTitle} 
-                        alt="developer"
-                    />
-                Logan
-                <br />
-                web developer
-                </h1>
-                <h2>what i do</h2>
-                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="developer"/>
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={15}/>
+
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={20}/>
+                    </h1>
+                    <h2>University Student / Developer</h2>
+                    <Link to='/contact' className='flat-button'>CONTACT ME</Link>
             </div>
     
         </div>
